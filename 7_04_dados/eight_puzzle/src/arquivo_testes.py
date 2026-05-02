5e19a988ed29623423f9c94c3428615d9b687111
KrLiam <krwilliam03@gmail.com>
2026-05-02 10:27:18 -0300

    vomita tudo

--- 7_04_dados/eight_puzzle/src/arquivo_testes.py
+++ 7_04_dados/eight_puzzle/src/arquivo_testes.py
@@ -6,95 +6,91 @@ from invalid_position_exception import InvalidPositionException
 
 
 class TesteAllCUsesSomePUses(unittest.TestCase):
-    pass
+    def test__c_uses_some_p_uses__line__0_1_4(self):
+        # Inline Fixture Setup
+        game = PuzzleGame(3)
+        TestingShufflerPuzzleGame3x3To12345X786().shuffle(game)
 
-    # def test__c_uses_some_p_uses__line__0_1_4(self):
-    #     # Inline Fixture Setup
-    #     game = PuzzleGame(3)
-    #     TestingShufflerPuzzleGame3x3To12345X786().shuffle(game)
+        # Exercise SUT
+        tile = game.get_tile(2, 2)
 
-    #     # Exercise SUT
-    #     tile = game.get_tile(2, 2)
+        # Result Verification
+        self.assertEqual(tile, 5)
 
-    #     # Result Verification
-    #     self.assertEqual(tile, 5)
+    def test__c_uses_some_p_uses__column__0_1_4(self):
+        # Inline Fixture Setup
+        game = PuzzleGame(3)
+        TestingShufflerPuzzleGame3x3To12345X786().shuffle(game)
 
-    # def test__c_uses_some_p_uses__column__0_1_4(self):
-    #     # Inline Fixture Setup
-    #     game = PuzzleGame(3)
-    #     TestingShufflerPuzzleGame3x3To12345X786().shuffle(game)
+        # Exercise SUT
+        tile = game.get_tile(3, 2)
 
-    #     # Exercise SUT
-    #     tile = game.get_tile(3, 2)
-
-    #     # Result Verification
-    #     self.assertEqual(tile, 8)
+        # Result Verification
+        self.assertEqual(tile, 8)
 
 class TesteAllPUses(unittest.TestCase):
-    pass
+    def test__all_p_uses__line__0_1_3(self):
+        # Inline Fixture Setup
+        game = PuzzleGame(3)
+        TestingShufflerPuzzleGame3x3To12345X786().shuffle(game)
 
-    # def test__all_p_uses__line__0_1_3(self):
-    #     # Inline Fixture Setup
-    #     game = PuzzleGame(3)
-    #     TestingShufflerPuzzleGame3x3To12345X786().shuffle(game)
+        # Exercise SUT
+        tile = game.get_tile(2, 3)
 
-    #     # Exercise SUT
-    #     tile = game.get_tile(2, 3)
+        # Result Verification
+        self.assertEqual(tile, ' ')
 
-    #     # Result Verification
-    #     self.assertEqual(tile, ' ')
+    def test__all_p_uses__line__0_1_4(self):
+        # Inline Fixture Setup
+        game = PuzzleGame(3)
+        TestingShufflerPuzzleGame3x3To12345X786().shuffle(game)
 
-    # def test__all_p_uses__line__0_1_4(self):
-    #     # Inline Fixture Setup
-    #     game = PuzzleGame(3)
-    #     TestingShufflerPuzzleGame3x3To12345X786().shuffle(game)
+        # Exercise SUT
+        tile = game.get_tile(1, 1)
 
-    #     # Exercise SUT
-    #     tile = game.get_tile(1, 1)
+        # Result Verification
+        self.assertEqual(tile, 1)
 
-    #     # Result Verification
-    #     self.assertEqual(tile, 1)
+    def test__all_p_uses__line__0_2(self):
+        # Inline Fixture Setup
+        game = PuzzleGame(3)
+        TestingShufflerPuzzleGame3x3To12345X786().shuffle(game)
 
-    # def test__all_p_uses__line__0_2(self):
-    #     # Inline Fixture Setup
-    #     game = PuzzleGame(3)
-    #     TestingShufflerPuzzleGame3x3To12345X786().shuffle(game)
-
-    #     # Exercise SUT
-    #     with self.assertRaises(InvalidPositionException):
-    #         game.get_tile(4, 1)
+        # Exercise SUT
+        with self.assertRaises(InvalidPositionException):
+            game.get_tile(4, 1)
 
 
-    # def test__all_p_uses__column__0_1_3(self):
-    #     # Inline Fixture Setup
-    #     game = PuzzleGame(3)
-    #     TestingShufflerPuzzleGame3x3To12345X786().shuffle(game)
+    def test__all_p_uses__column__0_1_3(self):
+        # Inline Fixture Setup
+        game = PuzzleGame(3)
+        TestingShufflerPuzzleGame3x3To12345X786().shuffle(game)
 
-    #     # Exercise SUT
-    #     tile = game.get_tile(2, 3)
+        # Exercise SUT
+        tile = game.get_tile(2, 3)
 
-    #     # Result Verification
-    #     self.assertEqual(tile, ' ')
+        # Result Verification
+        self.assertEqual(tile, ' ')
 
-    # def test__all_p_uses__column__0_1_4(self):
-    #     # Inline Fixture Setup
-    #     game = PuzzleGame(3)
-    #     TestingShufflerPuzzleGame3x3To12345X786().shuffle(game)
+    def test__all_p_uses__column__0_1_4(self):
+        # Inline Fixture Setup
+        game = PuzzleGame(3)
+        TestingShufflerPuzzleGame3x3To12345X786().shuffle(game)
 
-    #     # Exercise SUT
-    #     tile = game.get_tile(3, 3)
+        # Exercise SUT
+        tile = game.get_tile(3, 3)
 
-    #     # Result Verification
-    #     self.assertEqual(tile, 6)
+        # Result Verification
+        self.assertEqual(tile, 6)
 
-    # def test__all_p_uses__column__0_2(self):
-    #     # Inline Fixture Setup
-    #     game = PuzzleGame(3)
-    #     TestingShufflerPuzzleGame3x3To12345X786().shuffle(game)
+    def test__all_p_uses__column__0_2(self):
+        # Inline Fixture Setup
+        game = PuzzleGame(3)
+        TestingShufflerPuzzleGame3x3To12345X786().shuffle(game)
 
-    #     # Exercise SUT
-    #     with self.assertRaises(InvalidPositionException):
-    #         game.get_tile(1, 4)
+        # Exercise SUT
+        with self.assertRaises(InvalidPositionException):
+            game.get_tile(1, 4)
 
 
 if __name__ == "__main__":
