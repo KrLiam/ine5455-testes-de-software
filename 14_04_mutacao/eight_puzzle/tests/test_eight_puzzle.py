542f1c616e0793eb90680237b634385d9b8e96c9
KrLiam <krwilliam03@gmail.com>
2026-05-12 09:50:14 -0300

    sla comita vey

--- 14_04_mutacao/eight_puzzle/tests/test_eight_puzzle.py
+++ 14_04_mutacao/eight_puzzle/tests/test_eight_puzzle.py
@@ -92,6 +92,9 @@ class TesteAllPUses(unittest.TestCase):
 
 
 class TesteMutantes(unittest.TestCase):
+    pass
+
+    # # Mata mutante get_tile_4
     def test__line_0(self):
         # Inline Fixture Setup
         game = PuzzleGame(3)
@@ -100,6 +103,7 @@ class TesteMutantes(unittest.TestCase):
         with self.assertRaises(InvalidPositionException):
             game.get_tile(0, 1)
 
+    # # Mata mutante get_tile_7
     def test__column_0(self):
         # Inline Fixture Setup
         game = PuzzleGame(3)
@@ -108,7 +112,7 @@ class TesteMutantes(unittest.TestCase):
         with self.assertRaises(InvalidPositionException):
             game.get_tile(1, 0)
 
-    # Mata mutante 34
+    # # Mata mutante move_tile_34
     def test_ramo_0_1_2_3_4(self):
         # Inline Fixture Setup
         game = PuzzleGame(3)
@@ -120,7 +124,7 @@ class TesteMutantes(unittest.TestCase):
         # Result Verification
         self.assertEqual(sucesso, True)
     
-    # Mata mutante 3 e 35
+    # # Mata mutante move_tile_3 e move_tile_35
     def test_ramo_0_1_2_5(self):
         # Inline Fixture Setup
         game = PuzzleGame(3)
@@ -133,11 +137,12 @@ class TesteMutantes(unittest.TestCase):
         # Result Verification
         self.assertEqual(sucesso, False)
 
-    def test_move_tile_from_a_position_to_the_empty_position_returns_false(self):
-        game = PuzzleGame(3)
-        TestingShufflerPuzzleGame3x3To12345X786().shuffle(game)
-        result = game.move_tile_from_a_position_to_the_empty_position(-1, 0)
-        self.assertFalse(result)
+    # #
+    # def test_move_tile_from_a_position_to_the_empty_position_returns_false(self):
+    #     game = PuzzleGame(3)
+    #     TestingShufflerPuzzleGame3x3To12345X786().shuffle(game)
+    #     result = game.move_tile_from_a_position_to_the_empty_position(-1, 0)
+    #     self.assertFalse(result)
 
 if __name__ == "__main__":
     unittest.main()
